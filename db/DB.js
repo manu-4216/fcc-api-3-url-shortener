1e45319c827a42e5ff944cd696755da854a66374
Manuel M <manu4216@gmail.com>
2017-04-11 23:38:31 +0000

    Add forgoten folders

---
+++ db/DB.js
@@ -0,0 +1,119 @@
+/**************************************************************************
+ * Low level utilities for managing the DB (connect, close, update, find)
+ *    db schema: (_id, originalUrl, shortUrlSuffix)
+ **************************************************************************/
+
+var DB = function () {
+    var mongo = require('mongodb').MongoClient
+    var DB_NAME = 'urlshort'
+    var COLLECTION_NAME = 'urls'
+    //var DB_URL = 'mongodb://' + process.env.IP + ':27017/' + DB_NAME  // local db
+    //var DB_URL = require('../private/privateData').dbUrl  // mLab db
+    var DB_URL = process.env.dbUrl
+    var myDB
+    var myCollection
+    
+    console.log('dburl', DB_URL)
+    
+    var connect = function () {
+        return new Promise((resolve, reject) => {
+            //Reuse an open db connection.
+            if (myDB) {
+                console.log('DB already open. Return the old one.')
+                resolve(myDB)
+            }
+            
+            /*
+            var options = {
+                keepAlive: 1,
+                connectTimeoutMS: 30000
+            }
+            */
+            
+            /*
+            var options = {
+                server: {
+                    auto_reconnect:true, poolSize: 200,
+                    socketOptions: {
+                        connectTimeoutMS: 1000
+                    }
+                }
+            }
+            */
+            
+            var options = {
+                keepAlive: 1,
+                connectTimeoutMS: 30000
+            }
+            
+
+            mongo.connect(DB_URL, options,  function (err, db) {
+                if (err) {
+                    console.log('- rejected connect')
+                    reject(err)
+                } else {
+                    // Store DB and Collection
+                    myDB = db
+                    myCollection = db.collection(COLLECTION_NAME)
+                    resolve(db)
+                }
+            })
+        })
+    }
+    
+    var close = function () {
+        console.log('DB: closing')
+        if (myDB) {
+            myDB.close()
+        }
+    }
+    
+    var update = function (item) {
+        return myCollection.update(
+                { originalUrl: item.originalUrl }, // query by originalUrl, to make sure it's unique
+                item, 
+                { upsert: true }, // creates a new entry if does not exist
+                function (err) {
+                    if (err) throw err
+                
+                    //myDB.close()
+                    // myDB = null
+            })
+    }
+    
+    var find = function (item) {
+        return new Promise((resolve, reject) => {
+            myCollection.find(item).toArray()
+                .then(result => {
+                    console.log('DB: finding')
+                    /////close()
+                    resolve(result)
+                })
+                .catch(err => reject(err))
+        })
+    }
+    
+    var count = function () {
+        // Use custom promise, to be able to close the DB here, before resolving the native count() promise
+        return new Promise((resolve, reject) => {
+            myCollection.find().count()
+                .then(count => {
+                    console.log('DB: counting')
+                    /////close()
+                    resolve(count)
+                })
+                .catch(err => reject(err))
+        })
+    }
+    
+    
+    return {
+        connect: connect,
+        close: close,
+        update: update,
+        find: find,
+        count: count
+    }
+}()
+
+module.exports = DB
---
+++ db/utils.js
@@ -0,0 +1,76 @@
+var utils = function () {
+    var DB = require('../db/DB')
+    
+    /**
+     * Gets the short URL prefix: 
+     *   if new URL, generate it: count + 1. If existing in the DB, return it
+     */
+   function getShortUrlSuffix (originalUrl) {
+    console.log('GET URL SUFFIX')
+    return new Promise((resolve, reject) => {
+        DB.connect()
+        .then(db => {
+            // Get the stored originalUrl, if it exists
+            return getUrl({ originalUrl })
+        })
+        .then(results => {
+            if (results.length > 0) {
+                console.log("found, length = ", results.length)
+                resolve(results[0].shortUrlSuffix)
+            } else {
+                console.log('no result')
+                // If no result, the item will be added at the end, so get db.count()
+                DB.count()
+                    .then(count => {
+                        console.log('count', count)
+                        resolve(parseInt(count) + 1)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        reject(err)
+                    })
+            }
+        })
+        .catch(err => {
+            console.log('rejected getShortUrlSuffix', err)
+            reject(err)
+        })
+    })
+}
+
+/**
+ * Take an item, and put it in the DB
+ */
+function saveUrl (item) {
+    console.log('SAVE URL')
+    return DB.connect()
+        .then(db => {
+            DB.update(item)
+        })
+}
+
+/**
+ * Use the short url, and extract the original url
+ */
+function getUrl (query) {
+    console.log('GET URL')
+    return new Promise((resolve, reject) => {
+        DB.connect()
+        .then(db => {
+            resolve(DB.find(query))
+        })
+        .catch(err => {
+            reject(err)
+        })
+    })
+}
+
+
+return {
+    getShortUrlSuffix: getShortUrlSuffix,
+    saveUrl: saveUrl,
+    getUrl: getUrl
+}
+}()
+
+module.exports = utils
